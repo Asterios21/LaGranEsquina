a9204c279caf799ff63cadc1432c65032e7c41a1
Asterios21 <nihk2001@hotmail.com>
2022-09-18 19:26:52 -0500

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  config : {
+    apiKey: "AIzaSyDKNKcH7dwfdrD5sgeAB2r21mnIdNr50Cc",
+    authDomain: "lagranesquina-b7377.firebaseapp.com",
+    projectId: "lagranesquina-b7377",
+    storageBucket: "lagranesquina-b7377.appspot.com",
+    messagingSenderId: "90316140716",
+    appId: "1:90316140716:web:633f30f98290804e82ef09",
+    measurementId: "G-63J4143F8S"
+  }
+ 
+};
